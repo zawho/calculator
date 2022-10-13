b7a7c3f1a96244effb791916b61d8ef496a1bb57
zawho <zthooper2@gmail.com>
2022-10-13 10:33:56 +0800

    Add basic HTML and arithmetic funcs

---
+++ main.js
@@ -0,0 +1,16 @@
+// Calculator mathematical functions.
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
