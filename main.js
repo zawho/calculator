9633759d9c26b46ed9b2035bd3d0037d80c126f7
zawho <zthooper2@gmail.com>
2022-10-14 10:40:19 +0800

    Add basic operate func

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 //Arithmetic functions.
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a, b) {
-    return a - b;
+    return +a - +b;
 }
 
 function multiply(a, b) {
-    return a * b;
+    return +a * +b;
 }
 
 function divide(a, b) {
-    return a / b;
+    return +a / +b;
 }
+
+//Take user inputs and apply the arithmetic functions.
+function operate() {
+    let userNumA = prompt('Choose a number');
+    let userNumB = prompt('Choose another number');
+    let operatorVar = prompt ('Choose an operator');
+    let answerVar;
+    let calcString = `${userNumA} ${operatorVar} ${userNumB}`;
+    if (operatorVar === '+') {
+        answerVar = add(userNumA, userNumB);
+    } else if (operatorVar === '-') {
+        answerVar = subtract(userNumA, userNumB);
+    } else if (operatorVar === '*') {
+        answerVar = multiply(userNumA, userNumB);
+    } else if (operatorVar === '/') {
+        answerVar = divide(userNumA, userNumB);
+    }
+    console.log(`${calcString} = ${answerVar}`);
+}
+
+operate();
